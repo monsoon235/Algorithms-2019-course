874cf71428ad65985713362cf458eaa9c29a8f27
yjh1021317464 <yjh1021317464@gmail.com>
2019-12-08 23:34:17 +0800

    Update exp3

--- exp-3-Karp-Rabin.py
+++ exp-3-Karp-Rabin.py
@@ -1,47 +1,80 @@
 import random
 import string
+import time
+from functools import reduce
+
+import numpy as np
+import pandas as pd
 from typing import List
 
 
+# T 为待匹配串，P 为模式串
 def match_Karp_Rabin(T: str, P: str) -> List[int]:
     n = len(T)
     m = len(P)
-    assert n >= m > 0
-    rune_set = set(T + P)  # 所有 unicode 码点
-    d = len(rune_set)
-    rune_to_int = dict(zip(rune_set, range(d)))  # unicode 码点映射到 int
-    q = 999983  # 获取使得 dq 在 int32 表示范围内的较大整数
-    h = pow(d, m - 1, q)  # d**(m-1) % q
-    p = 0
-    t = 0
-    ret = []  # 匹配时 P 的偏移量
-    for i in range(m):
-        p = (d * p + rune_to_int[P[i]]) % q  # 计算代表 p 的数
-        t = (d * t + rune_to_int[T[i]]) % q  # 初始化 t
-    for shift in range(n - m + 1):  # shift 为当前偏移量
-        if p == t:
-            if all(P[j] == T[j + shift] for j in range(m)):  # 检测伪命中
-                print(f'Pattern occurs with shift {shift}')
-                ret.append(shift)
-        if shift < n - m:  # 更新 t
-            t = (d * (t - h * rune_to_int[T[shift]])
-                 + rune_to_int[T[shift + m]]) % q
-    return ret
+    if m > n:  # 模式串比待匹配串长，返回空
+        return []
+    rune_set = set(T + P)  # T 和 P 中所有 unicode 码点
+    d = len(rune_set)  # 出现的 unicode 数量
+    rune_to_int = dict(zip(rune_set, range(d)))  # 把 unicode 码点映射到 int
+    q = 999983  # q 为 使得 dq 在 int32 表示范围内的一个较大质数
+    h = pow(d, m - 1, q)  # h = d**(m-1) % q, 内置的 pow 函数可以更快速计算
+    offset = []  # 匹配时 P 相对于 T 的偏移量
+    # 初始化 p_hash t_hash
+    p_hash = reduce(lambda hash, c: (d * hash + rune_to_int[c]) % q, P, 0)  # p 为 P 的 hash 值
+    t_hash = reduce(lambda hash, c: (d * hash + rune_to_int[c]) % q, T[:m], 0)  # t 为 当前检测时 T 子串的 hash 值
+    # shift 为 P 相对于 T 开始的偏移量
+    for shift in range(n - m + 1):
+        if p_hash == t_hash:
+            # 检测伪命中
+            if all(P[j] == T[j + shift] for j in range(m)):
+                offset.append(shift)  # 不是伪命中，则加入到偏移量列表
+        if shift < n - m:  # 更新 t_hasp
+            # 此方法只需要 O(1) 的时间即可获得子串向右偏移一位时的新 hash
+            # 而不用耗费 O(m) 的时间重新计算
+            t_hash = (d * (t_hash - h * rune_to_int[T[shift]])
+                      + rune_to_int[T[shift + m]]) % q
+    return offset
 
 
 def get_random_str(n: int) -> str:
     assert n > 0
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(n))
+    return ''.join(random.choice(string.ascii_letters + string.digits)
+                   for _ in range(n))
+
+
+# 穷举法
+def get_std_answer(T: str, P: str) -> List[int]:
+    n = len(T)
+    m = len(P)
+    ret = []
+    for offset in range(n - m):
+        if T[offset:m + offset] == P:
+            ret.append(offset)
+    return ret  # 返回所有在 T 中的匹配位置
 
 
 if __name__ == '__main__':
-    T = get_random_str(2 ** 8)
-    P = get_random_str(8)
-    print(match_Karp_Rabin(T, P))
-    T = get_random_str(2 ** 11)
-    P = get_random_str(16)
-    print(match_Karp_Rabin(T, P))
-    T = get_random_str(2 ** 14)
-    P = get_random_str(32)
-    print(match_Karp_Rabin(T, P))
-    print(match_Karp_Rabin('333333333333333', '333'))
+    # 性能测试与正确性检验
+    n_list = list(range(10 ** 5, 10 ** 6 + 1, 10 ** 5))
+    m_list = list(range(10 ** 3, 5 * 10 ** 3 + 1, 10 ** 3))
+    time_cost = pd.DataFrame(
+        index=pd.Index(n_list, name='n'),
+        columns=pd.Index(m_list, name='m'),
+        dtype=float
+    )
+    for m in m_list:
+        for n in n_list:
+            T = get_random_str(n)
+            P = get_random_str(m)
+            start = time.time()
+            offsets = match_Karp_Rabin(T, P)
+            end = time.time()
+            assert offsets == get_std_answer(T, P), f'error occurs when n={n}, m={m}'
+            time_cost.loc[n, m] = end - start
+            print(f'n={n}, m={m}, time={end - start} s')
+    print(time_cost)
+    mn_matrix = np.array([
+        [m + n for m in m_list] for n in n_list
+    ])
+    print(time_cost * 10 ** 7 / mn_matrix)
--- exp-3-longest-common-subsequence.py
+++ exp-3-longest-common-subsequence.py
@@ -1,5 +1,7 @@
 import string
+import time
 import numpy as np
+import pandas as pd
 from enum import Enum, unique
 from typing import List
 import random
@@ -12,12 +14,15 @@ class Direction(Enum):
     Left_Up: str = '↖'
 
 
+# 获取 LCS
 def get_LCS(b: np.ndarray, s1: str, i: int, j: int, output: List[str]):
     if i == 0 or j == 0:
         return
+    # s1 和 s2 的最后一个字符相等的情况，则这个字符加入到 LCS
     if b[i, j] == Direction.Left_Up:
         get_LCS(b, s1, i - 1, j - 1, output)
-        output.append(s1[i-1])
+        output.append(s1[i - 1])
+    # 其他两种情况，分别去掉 s1 中的最后一个字符或 s2 中的最后一个字符，进行递归
     elif b[i, j] == Direction.Up:
         get_LCS(b, s1, i - 1, j, output)
     else:
@@ -25,18 +30,23 @@ def get_LCS(b: np.ndarray, s1: str, i: int, j: int, output: List[str]):
 
 
 def longest_common_subsequence(s1: str, s2: str) -> str:
-    m: int = len(s1)
-    n: int = len(s2)
+    m: int = len(s1)  # s1 长度
+    n: int = len(s2)  # s2 长度
+    # 初始化 b c 矩阵
     b = np.empty(shape=(len(s1) + 1, len(s2) + 1), dtype=Direction)
     c = np.empty(shape=(len(s1) + 1, len(s2) + 1), dtype=int)
+    # c 的递推边界设为 0
     c[:, 0] = 0
     c[0, :] = 0
+    # 计算 c[i, j]
     for i in range(1, m + 1):
         for j in range(1, n + 1):
+            # s1 和 s2 的最后一个字符相等，这个字符可能是 LCS 的字符
             if s1[i - 1] == s2[j - 1]:
                 c[i, j] = c[i - 1, j - 1] + 1
                 b[i, j] = Direction.Left_Up
                 # b[i, j] = '↖'
+            # 最后一个字符不相等，取 c[i-1,j] 和 c[i,j-1] 中较大的那个
             elif c[i - 1, j] >= c[i, j - 1]:
                 c[i, j] = c[i - 1, j]
                 b[i, j] = Direction.Up
@@ -46,14 +56,42 @@ def longest_common_subsequence(s1: str, s2: str) -> str:
                 b[i, j] = Direction.Left
                 # b[i, j] = '←'
     seq = []
-    get_LCS(b, s1, m, n, seq)
+    get_LCS(b, s1, m, n, seq)  # 用矩阵 b 获取 LCS
     return ''.join(seq)
 
 
+def get_random_str(n: int) -> str:
+    assert n > 0
+    return ''.join(random.choice(string.ascii_letters + string.digits)
+                   for _ in range(n))
+
+
 if __name__ == '__main__':
-    # s1 = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(2 ** 8))
-    # s2 = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(2 ** 6))
+    # 正确性测试
     s1 = '10010101'
     s2 = '010110110'
     seq = longest_common_subsequence(s1, s2)
     print(seq)
+
+    # 性能测试
+    m_list = list(range(200, 1501, 200))
+    n_list = list(range(200, 1501, 200))
+    time_cost = pd.DataFrame(
+        index=pd.Index(m_list, name='m'),
+        columns=pd.Index(n_list, name='n'),
+        dtype=float
+    )
+    for m in m_list:
+        for n in n_list:
+            s1 = get_random_str(m)
+            s2 = get_random_str(n)
+            start = time.time()
+            sub = longest_common_subsequence(s1, s2)
+            end = time.time()
+            time_cost.loc[m, n] = end - start
+            print(f'm={m}, n={n}, time={end - start} s')
+    print(time_cost)
+    mn_matrix = np.array([
+        [m * n for n in n_list] for m in m_list
+    ])
+    print(time_cost * 10 ** 6 / mn_matrix)
